59865ba4f98b97d438c886bb62d0bc460f137253
Souren Papazian <Souren Papazian>
2016-07-27 17:41:40 +0400

    1: Character Input

---
+++ character_input.py
@@ -0,0 +1,4 @@
+name = raw_input("what is your name: ")
+age = int(input("what is your age: "))
+number = int(100 - age)
+print("Hello " + name + " You are going to turn 100 years old in " + str(number) + " years")
