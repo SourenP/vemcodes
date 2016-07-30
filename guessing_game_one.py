3e10ec39b6e7d850b2e237a8dfbbf78e8e269354
vemboy <vem@papazian.info>
2016-07-30 20:08:36 +0400

    9: Guessing Game One

--- guessing_game_one.py
+++ guessing_game_one.py
@@ -1,13 +1,21 @@
+
+
+b = 3425245
 import random
 a = random.randint(1, 9)
-b = int(raw_input("guess the number YOU IMBECILE btw its 1-9: "))
-if a == b:
-    print "WTF man howd you do that YOU GUESSED IT CORRECT"
-elif b < a:
-    print "ya i thought so you were wrong its a bit bigger"
-elif b > a:
-    print "ya i thought so you were wrong its a bit smaller"
-elif b < 1 or b > 9:
-    print "do you know what guess the number YOU IMBECILE btw its 1-9 means?"
-else:
-    print "ya please dont put words"
+while(a != b):
+    b = raw_input("guess the number YOU IMBECILE btw its 1-9 or you can just type exit and the game will stop: ")
+    if b == "exit":
+        break
+    else:
+        b = int(b)
+    if a == b:
+        print "WTF man howd you do that YOU GUESSED IT CORRECT"
+    elif b < a:
+        print "ya i thought so you were wrong its a bit bigger"
+    elif b > a:
+        print "ya i thought so you were wrong its a bit smaller"
+    elif b < 1 or b > 9:
+        print "do you know what guess the number YOU IMBECILE btw its 1-9 means?"
+    else:
+        print "uhhhhhh just enter the numbers or break"
